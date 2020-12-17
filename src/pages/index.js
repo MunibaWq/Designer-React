5a31c8e53c7d4a9a4322d07c06ac70552252c682
Muniba Waqar <muniba.wq@gmail.com>
2020-12-16 18:48:48 -0700

    finished basic styling of classes

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,10 +11,10 @@ const IndexPage = () => (
       {/* HeroGroup div is gonna allow us to position the content and give us more freedom to make it adaptive later. */}
       <div className="HeroGroup">
         <SEO title="Home" />
-        <h1>Learn to Design and Code React Apps</h1>
+        <h1>React App with an Emphasis on Design and Adaptive Layouts</h1>
         <p>This is my new React website using gatsby.</p>
         <p>Now I will build something great.</p>
-        <Link to="/page-2/">Go to page 2</Link> <br />
+        <a> WATCH THE VIDEO</a> <br />
       </div>
     </div>
   </Layout>
