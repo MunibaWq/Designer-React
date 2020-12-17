aed473220b1b65fffcff78f2ffb3e2316930c00f
Muniba Waqar <muniba.wq@gmail.com>
2020-12-16 18:33:47 -0700

    update images folder and style Hero class in layout.css

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,15 +7,16 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Learn to Design and Code React Apps</h1>
-    <p>This is my new React website using gatsby.</p>
-    <p>Now I will build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="Hero">
+      {/* HeroGroup div is gonna allow us to position the content and give us more freedom to make it adaptive later. */}
+      <div className="HeroGroup">
+        <SEO title="Home" />
+        <h1>Learn to Design and Code React Apps</h1>
+        <p>This is my new React website using gatsby.</p>
+        <p>Now I will build something great.</p>
+        <Link to="/page-2/">Go to page 2</Link> <br />
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
