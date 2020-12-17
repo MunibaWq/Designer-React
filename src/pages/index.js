2462429ba4baf465f1f4f81b364fa0918fb498a6
Muniba Waqar <muniba.wq@gmail.com>
2020-12-16 20:52:59 -0700

    interactions and animations - applu mouse over effect on button

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,10 @@ const IndexPage = () => (
       {/* HeroGroup div is gonna allow us to position the content and give us more freedom to make it adaptive later. */}
       <div className="HeroGroup">
         <SEO title="Home" />
-        <h1>React App with an Emphasis on Design and Adaptive Layouts</h1>
+        <h1>
+          React App <br />
+          with an Emphasis on Design and Adaptive Layouts
+        </h1>
         <p>This is my new React website using gatsby.</p>
         <p>Now I will build something great.</p>
         <a> WATCH THE VIDEO</a> <br />
